11484d6022e854d6afe9b9004504eb6cb0ff2372
yuliakapchik <lulu-sun@mail.ru>
2020-07-21 01:45:26 +0300

    create: added keyboard

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 var mySwiper = new Swiper(".swiper-container", {
   // Optional parameters
   loop: true,
-
+  keyboard: {
+    enabled: true,
+    pageUpDown: true,
+  },
   // Navigation arrows
   navigation: {
     nextEl: ".slider-button--next",
