16685957a5eead2689f2829e6b1389c960c59e40
yuliakapchik <lulu-sun@mail.ru>
2020-07-25 13:44:09 +0300

    create: added API card

--- js/main.js
+++ js/main.js
@@ -10,9 +10,7 @@ var hotelSlider = new Swiper(".hotel-slider", {
     nextEl: ".hotel-slider__button--next",
     prevEl: ".hotel-slider__button--prev",
   },
-
   effect: "fade",
-
   speed: 800,
 });
 
@@ -28,10 +26,28 @@ var reviewsSlider = new Swiper(".reviews-slider", {
     nextEl: ".reviews-slider__button--next",
     prevEl: ".reviews-slider__button--prev",
   },
-
   speed: 600,
 });
 
+// параллакс-эффект
 $(".newsletter-parallax").parallax({
-  imageSrc: "../img/newsletter-background.jpg",
+  imageSrc: "./img/newsletter-background.jpg",
 });
+
+// API Яндекс карта
+// Дождёмся загрузки API и готовности DOM.
+ymaps.ready(init);
+function init() {
+  var myMap = new ymaps.Map("map-hotel", {
+    center: [12.934923151175724, 100.88307986706273],
+    zoom: 15,
+  });
+  // Метка
+  var placemark = new ymaps.Placemark(
+    [12.934923151175724, 100.88307986706273],
+    {
+      hintContent: "Hilton Hotel",
+    }
+  );
+  myMap.geoObjects.add(placemark);
+}
