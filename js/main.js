3083441553aee042c95b9510cfdfc7c104ac77dd
yuliakapchik <lulu-sun@mail.ru>
2020-08-01 16:01:37 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -51,3 +51,11 @@ function init() {
   );
   myMap.geoObjects.add(placemark);
 }
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log("Клик по кнопке меню");
+  document
+    .querySelector(".navbar-bottom")
+    .classList.toggle("navbar-bottom--visible");
+});
