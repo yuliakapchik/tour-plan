61ff544149fe64e100cc21e9fe8707af643bbc1a
yuliakapchik <lulu-sun@mail.ru>
2020-08-02 14:21:36 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -54,7 +54,6 @@ function init() {
 
 var menuButton = document.querySelector(".menu-button");
 menuButton.addEventListener("click", function () {
-  console.log("Клик по кнопке меню");
   document
     .querySelector(".navbar-bottom")
     .classList.toggle("navbar-bottom--visible");
